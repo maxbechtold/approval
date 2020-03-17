a115858521abd5a89e311294de6f2be80b13752d
Max Bechtold <maxbechtold@web.de>
2020-03-17 14:40:02 +0100

    Fix license header

--- approval-core/src/test/java/com/github/approval/reporters/JUnitReporterTest.java
+++ approval-core/src/test/java/com/github/approval/reporters/JUnitReporterTest.java
@@ -2,38 +2,6 @@ package com.github.approval.reporters;
 
 /*
  * #%L
- * com.github.maxbechtold:approval-core
- * %%
- * Copyright (C) 2014 - 2020 Nikolavp
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
-
-import static org.assertj.core.api.Assertions.*;
-
-import java.io.File;
-
-import org.assertj.core.util.Files;
-import org.junit.Test;
-import org.opentest4j.TestAbortedException;
-
-import com.github.approval.utils.ApprovalScriptWriter;
-import com.google.common.base.Charsets;
-
-/*
- * #%L
  * approval
  * %%
  * Copyright (C) 2020 Max Bechtold
@@ -51,6 +19,17 @@ import com.google.common.base.Charsets;
  * limitations under the License.
  * #L%
  */
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.File;
+
+import org.assertj.core.util.Files;
+import org.junit.Test;
+import org.opentest4j.TestAbortedException;
+
+import com.github.approval.utils.ApprovalScriptWriter;
+import com.google.common.base.Charsets;
 
 public class JUnitReporterTest {
 
